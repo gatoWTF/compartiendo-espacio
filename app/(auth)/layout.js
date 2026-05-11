b856c3b6f1b650c58e92699de80d800b4fb27536
Jeff <jeffersonrojasmeza15@hotmail.com>
2026-05-10 22:07:26 -0400

    mevolviloco

--- app/(auth)/layout.js
+++
@@ -1,7 +0,0 @@
-export default function AuthLayout({ children }) {
-  return (
-    <div style={{ width: '100%', height: '100vh', margin: 0, padding: 0, overflow: 'hidden' }}>
-      {children}
-    </div>
-  );
-}
